b49ab6f0921b341cfdfa3835e1953fc5503f706e
egoryaynov <egor.goryaynov02@gmail.com>
2021-02-07 21:27:06 +0400

    Clear project

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
 import React from 'react';
-import './App.scss';
-import Header from "./components/Header/Header";
-import Preloader from "./components/common/Preloader";
-import Main from "./components/Main/Main";
 
-function App({theme, setTheme, importTheme, getWeatherByCity}) {
-    const [isLoading, setIsLoading] = React.useState(true);
-
-    React.useEffect(() => {
-        importTheme()
-            .then(() => setIsLoading(false));
-    }, [importTheme])
-
-    const onChangeTheme = () => {
-        if (theme === "light") setTheme("dark");
-        if (theme === "dark") setTheme("light");
-
-        document.location.reload();
-    }
-
-    if (isLoading) return <Preloader/>
-
+function App() {
     return (
-        <div className="app">
-            <div className="container">
-                <div className="app-inner">
-                    <Header theme={theme} onChangeTheme={onChangeTheme}/>
-                    <Main getWeatherByCity={getWeatherByCity}/>
-                </div>
-            </div>
+        <div>
+            <h1>Version 2.0 coming soon....</h1>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from "react-router-dom";
 import App from './App';
-import themeLocalStorage from "./localStorage/themeLocalStorage";
-import {getWeatherByCity} from "./api/api";
-
-
-import "./index.scss";
-
-const importTheme = async () => {
-    if (themeLocalStorage.getTheme() === "light") return import("./scss/theme-light.scss");
-    if (themeLocalStorage.getTheme() === "dark") return import("./scss/theme-dark.scss");
-}
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App theme={themeLocalStorage.getTheme()}
-                 importTheme={importTheme}
-                 setTheme={themeLocalStorage.setTheme}
-                 getWeatherByCity={getWeatherByCity}/>
-        </BrowserRouter>
+        <App/>
     </React.StrictMode>,
     document.getElementById('root')
 );
