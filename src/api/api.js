4c9a1237c43d0e4f195d09a309d60ded75a37ab0
egoryaynov <egor.goryaynov02@gmail.com>
2021-02-21 00:39:54 +0400

    Deploy on gh pages

--- src/api/api.js
+++ src/api/api.js
@@ -2,20 +2,20 @@ const API_KEY = "02aa4385a4870233b2daa4f663c30d92";
 
 export const getCurrentWeatherByCity = async (city) => {
     const response = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
 
     return await response.json();
 }
 export const getCurrentWeatherByID = async (cityID) => {
     const response = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?id=${cityID}&appid=${API_KEY}&units=metric`);
+        `https://api.openweathermap.org/data/2.5/weather?id=${cityID}&appid=${API_KEY}&units=metric`);
 
     return await response.json();
 }
 
 export const get5DaysForecastByID = async (cityID) => {
     const response = await fetch(
-        `http://api.openweathermap.org/data/2.5/forecast?id=${cityID}&appid=${API_KEY}&units=metric&cnt=48`);
+        `https://api.openweathermap.org/data/2.5/forecast?id=${cityID}&appid=${API_KEY}&units=metric&cnt=48`);
 
     return await response.json();
 }
